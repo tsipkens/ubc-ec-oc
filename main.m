a4f126f6bf80fdf901cd352c1d4c9e8e042b0df7
Timothy A. Sipkens <tsipkens@gmail.com>
2020-09-18 15:47:40 -0700

    Reintroduced rgb2* funcs.

--- main.m
+++ main.m
@@ -5,9 +5,9 @@ clc;
 
 files = dir(fullfile('imgs\sample','*.jpg'));
 
-rgb = detect(files); % get RGB of central filters using image recog.
+rgb = detect(files(1:5)); % get RGB of central filters using image recog.
 
-[ec,oc] = ecoc.qu(rbg); % compute EC/OC using qu function
+[ec,oc] = ecoc.qu(rgb); % compute EC/OC using qu function
 
 ec_oc = ec./oc;
 
