e85c36d2511bbed6da7f008f741519f932353238
Timothy A. Sipkens <tsipkens@gmail.com>
2020-09-18 19:37:32 -0700

    Update main_train.m

--- main_train.m
+++ main_train.m
@@ -9,43 +9,63 @@ ec = rgb(:, 4);
 oc = rgb(:, 5);
 rgb = rgb(:, 1:3);
 
+xyz = rgb2xyz(rgb);
 lab = rgb2lab(rgb);
 lch = rgb2lch(rgb);
 
 % p1 = pca([rgb,lab,lch,ec]);
 % p2 = pca([rgb,lab,lch,oc]);
 
-x = [rgb, lab, lch, ec./oc];
-[coeffs, score, roots] = pca(x); % principle component analysis
+b_name = 'oc';
+b = eval(b_name);
+% A = [ones(size(rgb,1),1), rgb(:,1)];
+A = [ones(size(rgb,1),1), rgb, lab, ...
+    lch(:, 2:end), lab(:,2)./lab(:,3)];
+A = [ones(size(rgb,1),1), rgb(:,1), lab, ...
+    lch(:, 2:end), lab(:,2)./lab(:,3)];
 
-pct = roots ./ sum(roots) % percent explained by each basis
+f_pca = 0;
+if f_pca
+    [coeffs, score, roots] = pca(A(:,2:end)); % principle component analysis
+    pct = roots ./ sum(roots) % percent explained by each basis
+    A = [ones(size(rgb,1),1), score];
+end
 
-basis = coeffs(:, 1:(end-1)); % basis functions
-normal = coeffs(:, end); % normal vector
+
+
+% A = (score1'*score1) \ score1' * (ec./oc);
+x = (A'*A) \ A' * b;
+
+if f_pca
+    trnc = 5;
+    x(trnc+1:end) = 0;
+end
+
+b1 = A * x;
+
+out = [b, b1]
+x
+
+
+figure(1);
+plot(b, b1, '.');
+hold on;
+xlims = xlim;
+plot(xlim, xlim);
+hold off;
+xlabel(b_name);
+ylabel([b_name, ' fit']);
+
+
+figure(2);
+t0 = abs(corrcov(cov([A(:,2:end),b])));
+imagesc(t0);
+colormap(gray);
 
 
 r0 = 1:25:251;
 g0 = 1:10:251;
 [r1,g1] = ndgrid(r0,g0);
-
-
-% Truncate the number of bases considered
-trnc = 3; % truncate at this number of bases to include
-basis1 = basis(:, 1:trnc);
-score1 = score(:, 1:trnc);
-
-[n,p] = size(x);
-x_mean = mean(x, 1);
-x_fit = repmat(x_mean,n,1) + score1*basis1';
-residuals = x(:,end) - x_fit(:,end);
-
-error = abs(residuals);
-sse = sum(error.^2)
-pct_error = error ./ x(:,end);
-out = [error, pct_error]
-
-figure(1);
-loglog(ec ./ oc, x_fit(:,end), '.');
 
 
 
